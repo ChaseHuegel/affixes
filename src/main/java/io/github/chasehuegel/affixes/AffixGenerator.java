87d1a073ce4b10dafb40b8210d7cbede595c90fd
Seven <chase@chasehuegel.io>
2025-06-03 01:01:11 -0400

    Initial commit

---
+++ src/main/java/io/github/chasehuegel/affixes/AffixGenerator.java
@@ -0,0 +1,24 @@
+﻿package io.github.chasehuegel.affixes;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.EquipmentSlotGroup;
+import org.bukkit.inventory.ItemStack;
+
+public class AffixGenerator {
+
+    public void AddAffix(ItemStack item) {
+        var meta = item.getItemMeta();
+
+        var attribute = Attribute.ATTACK_DAMAGE;
+
+        var key = new NamespacedKey("io.github.chasehuegel.affixes", "1");
+        var amount = 1.0f;
+        var operation = AttributeModifier.Operation.MULTIPLY_SCALAR_1;
+        var slot = EquipmentSlotGroup.MAINHAND;
+        var modifier = new AttributeModifier(key, amount, operation, slot);
+
+        meta.addAttributeModifier(attribute, modifier);
+    }
+}
---
+++ src/main/java/io/github/chasehuegel/affixes/Affixes.java
@@ -0,0 +1,16 @@
+package io.github.chasehuegel.affixes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Affixes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
